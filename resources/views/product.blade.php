4e6cb824a64d956990aadb55f609015a7490cc8c
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-02 14:48:42 +0200

    product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,6 +2,15 @@
 
 @section('page.main')
     <div class="container">
-        
+        <article class="d-flex flex-wrap gap-5">
+            @foreach ($series as $serie)
+                <div class="card" style="flex-basis: 12%">
+                    <img src="{{ $serie['thumb'] }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">{{ $serie['series'] }}</h3>
+                    </div>
+                </div>
+            @endforeach
+        </article>
     </div>
 @endsection
