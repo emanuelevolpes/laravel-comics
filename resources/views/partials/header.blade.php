41c94044e326f797fbcbabc909ba72c2d077fefb
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-02 14:24:24 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <div class="menu">
+            <nav>
+                <ul class="list-unstyled d-flex justify-content-center align-items-center gap-3 py-3">
+                    <li><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" srcset=""></li>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="{{ route('home') }}">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
+    $data = [
+        'series' => config('comics')
+    ];
+
+    return view('home', $data);
+})-> name('home');
